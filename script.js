71428f61be1384d219fe46c2ec8e71af3e2a9854
Aveiro <matheusaveirofagundes@gmail.com>
2023-10-06 11:35:54 -0300

    initial commit

--- script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('input[type="text"][placeholder="CPF"]').mask('000.000.000-00', { reverse: true });
-    $('input[type="tel"]').mask('(00) 00000-0000');
-    $('input[type="text"][placeholder="CEP"]').mask('00000-000');
-});
