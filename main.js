ee2ffdbdc4a63d2e9fb55f0d6325509fe525ed0b
Aveiro <matheusaveirofagundes@gmail.com>
2023-12-06 08:06:42 -0300

    added subclass

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function PersonagemOnePiece(nome, tripulacao, frutaDoDiabo) {
     this.tripulacao = tripulacao;
     this.frutaDoDiabo = frutaDoDiabo;
     this.apresentar = function() {
-        console.log(`Yo-ho-ho! Eu sou o pirata ${this.nome}, da tripulação dos ${this.tripulacao}!`);
+        console.log(`Yo-ho-ho! Eu sou o pirata ${this.nome}, membro da tripulação dos ${this.tripulacao}!`);
     }
     this.realizarAcao = function(acao) {
         console.log(`${this.nome} realizando ação pirata: ${acao}!`);
@@ -24,7 +24,18 @@ function MembroTripulacao(nome, tripulacao, frutaDoDiabo, cargo) {
     PersonagemOnePiece.call(this, nome, tripulacao, frutaDoDiabo);
 }
 
-const luffy = new MembroTripulacao("Monkey D. Luffy", "Chapéu de Palha", "Gomu Gomu no Mi", "Capitão");
+function Capitao(nome, tripulacao, frutaDoDiabo) {
+    MembroTripulacao.call(this, nome, tripulacao, frutaDoDiabo, "Capitão");
+
+    this.tomarDecisao = function(decisao) {
+        console.log(`Capitão ${this.nome} tomando decisão: ${decisao}!`);
+    }
+    this.apresentar = function() {
+        console.log(`Yo-ho-ho! Eu sou o pirata ${this.nome}, capitão da tripulação dos ${this.tripulacao}!`);
+    }
+}
+
+const luffy = new Capitao("Monkey D. Luffy", "Chapéu de Palha", "Gomu Gomu no Mi");
 const zoro = new MembroTripulacao("Roronoa Zoro", "Chapéu de Palha", "-", "Espadachim");
 const nami = new MembroTripulacao("Nami", "Chapéu de Palha", "-", "Navegadora");
 
@@ -34,8 +45,10 @@ console.log(luffy.obterInformacoes());
 console.log(zoro.obterInformacoes());
 console.log(nami.obterInformacoes());
 console.log();
+
 luffy.apresentar();
 luffy.realizarAcao("Esticar os braços");
+luffy.tomarDecisao("Partiremos para a próxima ilha!");
 console.log();
 zoro.apresentar();
 zoro.realizarAcao("Treinar com espadas");
