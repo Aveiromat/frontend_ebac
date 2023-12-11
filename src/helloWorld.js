b6c9ab19278ee27ee784d8c494cf9d95a6972972
Matheus Aveiro <Matheusaveirofagundes@gmail.com>
2023-12-11 19:55:12 -0300

    initial commit

---
+++ src/helloWorld.js
@@ -0,0 +1,12 @@
+"use strict";
+function multiplicacao(x, y) {
+    return +x * y;
+}
+const resultadoMultiplicacao = multiplicacao(5, 3);
+console.log(resultadoMultiplicacao);
+//
+function saudacao(nome) {
+    return "Hello " + nome;
+}
+const mensagemSaudacao = saudacao("World!");
+console.log(mensagemSaudacao); // Saída: Olá João
---
+++ src/helloWorld.ts
@@ -0,0 +1,14 @@
+function multiplicacao(x: number, y: number):number {
+    return + x * y;
+}
+
+const resultadoMultiplicacao = multiplicacao(5, 3);
+console.log(resultadoMultiplicacao);
+//
+function saudacao(nome: string): string {
+    return "Hello " + nome;
+}
+
+const mensagemSaudacao = saudacao("World!");
+console.log(mensagemSaudacao); // Saída: Olá João
+
